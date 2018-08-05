42b1bad61e0082222f52bdde7a9a229ab0c3d23b
Cheitovilla <cheitovilla@outlook.com>
2018-08-04 19:35:51 -0500

    Player_Listo
    Creacion del player y animación del mismo

---
+++ Nediar/Assets/Scripts/FPSCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour {
+
+	public float mouseX;
+	public float mouseY;
+	public float secibility = 50f;
+	public bool ShowCursor;
+
+	public bool InvertedMouse;
+
+	void Update()
+	{
+		MovMause();
+	}
+
+	void MovMause()
+	{
+		Camera cam = Camera.main;
+		mouseX += (Input.GetAxis("Mouse X") * secibility) * Time.deltaTime;
+		mouseY -= (Input.GetAxis("Mouse Y") * secibility) * Time.deltaTime;
+		transform.eulerAngles = new Vector3(0, mouseX, 0);
+		cam.transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	
+	}
+}
---
+++ Nediar/Assets/Scripts/FPSMove.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMove : MonoBehaviour 
+{
+
+	public float speed;
+	public float jumpForce;
+	Rigidbody rb;
+	Animator anim;
+	public bool enSuelo = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		anim = GetComponent<Animator> ();
+		anim.SetTrigger ("idle");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			anim.SetTrigger ("walkup");
+			transform.position += transform.forward * speed * Time.deltaTime;
+
+			if (Input.GetKey (KeyCode.Space)) 
+			{
+				if (enSuelo) 
+				{
+					enSuelo = false;
+					anim.SetTrigger ("jump");
+					transform.position += transform.up * speed * Time.deltaTime;	
+					rb.velocity = new Vector3 (rb.velocity.x, jumpForce, rb.velocity.z);
+				}
+			} 
+		} 
+		else if (Input.GetKey (KeyCode.S)) 
+		{
+			anim.SetTrigger ("walkback");
+			transform.position -= transform.forward * speed * Time.deltaTime;
+
+			if (Input.GetKey (KeyCode.Space)) 
+			{
+				if (enSuelo) 
+				{
+					enSuelo = false;
+					anim.SetTrigger ("jump");
+					transform.position += transform.up * speed * Time.deltaTime;	
+					rb.velocity = new Vector3 (rb.velocity.x, jumpForce, rb.velocity.z);
+				}
+			} 
+		}
+
+		else if (Input.GetKey (KeyCode.Space)) 
+		{
+			if (enSuelo) 
+			{
+				enSuelo = false;
+				anim.SetTrigger ("jump");
+				transform.position += transform.up * speed * Time.deltaTime;	
+				rb.velocity = new Vector3 (rb.velocity.x, jumpForce, rb.velocity.z);
+			}
+		} 
+
+		else if (Input.GetButtonDown ("Fire1")) 
+		{
+			anim.SetTrigger ("attack");	
+		}
+
+		else 
+		{
+			anim.SetTrigger ("idle");	
+		}
+	}
+
+
+		private void OnCollisionEnter (Collision collision)
+		{
+			if (collision.gameObject.tag == "Piso") 
+			{
+				enSuelo = true;	
+			}
+		}
+}
+
