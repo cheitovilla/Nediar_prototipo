74f83ed5b72166d067791e1850cf5716e7312d14
Cheitovilla <cheitovilla@outlook.com>
2018-08-06 12:52:56 -0500

    pause e menu

---
+++ Nediar/Assets/Scripts/AdminScenes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdminScenes : MonoBehaviour {
+
+	public void PlayGame(){
+		
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void ExitAplication(){
+		
+		Application.Quit ();
+	}
+
+	public void SceneInitial()
+	{
+		//SceneManager.LoadScene
+	}
+
+}
---
+++ Nediar/Assets/Scripts/Pause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject panel_pause;
+
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		Time.timeScale = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Pausa ();
+		}
+	}
+
+
+	public void Pausa(){
+		panel_pause.SetActive (true);
+
+		Time.timeScale = 0;
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.Confined;
+
+	}
+
+	public void Reanudar(){
+
+		panel_pause.SetActive (false);
+		Time.timeScale = 1;
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+
+	}
+
+	public void VolverInicio(){
+		SceneManager.LoadScene ("Inicio");
+	}
+}
